dce7a0bf0d49e10afddc0d717b091ef15de0472e
Goldfortwin <38490311+Goldfortwin@users.noreply.github.com>
2018-04-24 19:41:54 +0200

    Scripts nombre de joueur

---
+++ ScriptsNombreJoueur/NJBack.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Experimental.UIElements;
+
+public class NJBack : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        UnityEngine.UI.Button retour = GetComponent<UnityEngine.UI.Button>();
+        retour.onClick.AddListener(TaskOnClick);
+    }
+
+    void TaskOnClick()
+    {
+        Application.LoadLevel(0);
+
+    }
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
