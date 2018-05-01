1847c040354de742932a3616f61e092ce4d8ab98
Unknown <ramesh.viramouttou@edu.itescia.fr>
2018-05-01 17:41:03 +0200

    image

---
+++ Assets/ScriptsAccueil/AccueilControl.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Experimental.UIElements;
+
+public class AccueilControl : MonoBehaviour {
+    //public UnityEngine.UI.Button newGame;
+    // Use this for initialization
+    void Start () {
+        UnityEngine.UI.Button newGame = GetComponent<UnityEngine.UI.Button>();
+        newGame.onClick.AddListener(TaskOnClick);
+    }
+
+    void TaskOnClick()
+    {
+        Application.LoadLevel(2);
+
+    }
+
+	// Update is called once per frame
+	void Update () {
+    
+	}
+}
---
+++ Assets/ScriptsNombreJoueur/NumberChoice.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Experimental.UIElements;
+
+public class NumberChoice : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        UnityEngine.UI.Button choix = GetComponent<UnityEngine.UI.Button>();
+        choix.onClick.AddListener(TaskOnClick);
+    }
+
+    void TaskOnClick()
+    {
+        Application.LoadLevel(1);
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
